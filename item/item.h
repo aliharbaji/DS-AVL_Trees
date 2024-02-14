27ad53787dba69b79508a21cf36002e54737d5e5
Ali <alli.harbaji@gmail.com>
2024-02-14 22:26:35 +0200

    first commit, made Node and Item classes, might change the implementation to a non-generic implementation

---
+++ item/item.h
@@ -0,0 +1,35 @@
+//
+// Created by allih on 14/02/2024.
+//
+
+#ifndef AVLTREES_ITEM_H
+#define AVLTREES_ITEM_H
+
+#include "node.h"
+
+class Item {
+    int ID;
+public:
+    explicit Item(int ID) : ID(ID) {}
+
+    int getID() const {
+        return ID;
+    }
+
+    virtual ~Item() = default;
+};
+
+class Team : public Item{
+    int numberOfContestants;
+    int strength;
+public:
+    explicit Team(int ID) : Item(ID), numberOfContestants(Zero), strength(Zero) {}
+    // country*
+    // allContestants*
+};
+
+
+
+
+
+#endif //AVLTREES_ITEM_H
---
+++ tests/test2.h
@@ -0,0 +1,14 @@
+//
+// Created by allih on 14/02/2024.
+//
+
+#ifndef AVLTREES_TEST2_H
+#define AVLTREES_TEST2_H
+
+
+class test2 {
+
+};
+
+
+#endif //AVLTREES_TEST2_H
