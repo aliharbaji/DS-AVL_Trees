810315cb9ad942d3d8f0386f1d303d4f4e150b62
Omar Jabarin <omar.ja@campus.technion.ac.il>
2024-02-18 08:50:56 +0200

    small bugfix and more tests

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
     //Test case 1:
-
+/*
     auto One = make_shared<Item>(1);
     auto Two = make_shared<Item>(2);
     auto Three = make_shared<Item>(3);
@@ -29,9 +29,46 @@ int main() {
 
     tree.remove(2);
     hasTwoTest = tree.contains(2);
+*/
 
 
+    Tree<Item> tree;
+    /* Test case 2: remove with multiple rotations including at the root
+    auto a1 = make_shared<Item>(50);
+    tree.insert(a1);
+    auto a2 = make_shared<Item>(25);
+    tree.insert(a2);
+    auto a3 = make_shared<Item>(75);
+    tree.insert(a3);
+    auto a4 = make_shared<Item>(15);
+    tree.insert(a4);
+    auto a5 = make_shared<Item>(40);
+    tree.insert(a5);
+    auto a6 = make_shared<Item>(60);
+    tree.insert(a6);
+    auto a7 = make_shared<Item>(80);
+    tree.insert(a7);
+    auto a8 = make_shared<Item>(35);
+    tree.insert(a8);
+    auto a9 = make_shared<Item>(55);
+    tree.insert(a9);
+    auto a10 = make_shared<Item>(65);
+    tree.insert(a10);
+    auto a11 = make_shared<Item>(90);
+    tree.insert(a11);
+    auto a12 = make_shared<Item>(62);
+    tree.insert(a12);
+    tree.remove(15);
+*/
 
+/*
+    auto a1 = make_shared<Item>(2);
+    tree.insert(a1);
+    auto a2 = make_shared<Item>(4);
+    tree.insert(a2);
+    auto a3 = make_shared<Item>(3);
+    tree.insert(a3);
+*/
 
     return 0;
 }
